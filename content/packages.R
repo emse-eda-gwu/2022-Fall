e4c031d3f4a13b588e1073f90ac686f4643c1906
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2022-08-17 18:06:48 +0000

    Built site for gh-pages

---
+++ content/packages.R
@@ -0,0 +1,15 @@
+# Install packages - only need to do this once!
+
+install.packages(c(
+    "cowplot",
+    "ggrepel",
+    "here",
+    "janitor",
+    "kableExtra",
+    "knitr",
+    "quarto",
+    "remotes",
+    "rmarkdown",
+    "tidyverse",
+    "viridis"
+))
