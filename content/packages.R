1c19856857e948dd59c0b66189fc3c14076d076f
John Helveston <john.helveston@gmail.com>
2022-08-22 14:58:55 -0400

    Update packages.R

--- content/packages.R
+++ content/packages.R
@@ -1,15 +1,48 @@
-# Install packages - only need to do this once!
-
+# Install packages from CRAN - only need to do this once!
 install.packages(c(
-    "cowplot",
-    "ggrepel",
-    "here",
-    "janitor",
-    "kableExtra",
-    "knitr",
-    "quarto",
-    "remotes",
-    "rmarkdown",
-    "tidyverse",
-    "viridis"
+    'DT',
+    'GGally',
+    'HistData',
+    'cowplot',
+    'gganimate',
+    'ggrepel',
+    'ggrepel',
+    'ggridges',
+    'here',
+    'hrbrthemes',
+    'htmltools',
+    'htmlwidgets',
+    'janitor',
+    'janitor',
+    'kableExtra',
+    'knitr',
+    'leaflet',
+    'lubridate',
+    'magick',
+    'mapproj',
+    'maps',
+    'palmerpenguins',
+    'plotly',
+    'quarto',
+    'reactable',
+    'reactablefmtr',
+    'remotes',
+    'rgeos',
+    'rmarkdown',
+    'rmdformats',
+    'rnaturalearth',
+    'rsconnect',
+    'sf',
+    'shiny',
+    'shinyWidgets',
+    'sparkline',
+    'tidyverse',
+    'viridis',
+    'viridis',
+    'waffle'
 ))
+
+# Install remote (development) packages
+remotes::install_github("mattcowgill/ggannotate")
+remotes::install_github("ropensci/rnaturalearthhires")
+remotes::install_github("ropensci/rnaturalearthdata")
